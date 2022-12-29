7b798682ed04716cf31eb346cecc059f6af419f0
mryevhenii07 <mrit.works07@gmail.com>
2022-12-29 16:38:06 +0200

    finished

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import { Switch, Route } from 'react-router-dom';
 import Header from '../../components/Header/Header';
-
 import AboutUsPage from '../../pages/AboutUsPage/AboutUsPage';
 import HomePage from '../../pages/HomePage/HomePage';
 import LoginPage from '../../pages/LoginPage/LoginPage';
